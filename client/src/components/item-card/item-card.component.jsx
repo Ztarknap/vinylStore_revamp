c8b4bba2dac7ff1b204e67419f4b7a2bff487e37
ztarknap <ztarknap>
2024-01-11 18:40:45 +0700

    browse skeleton

--- client/src/components/item-card/item-card.component.jsx
+++ client/src/components/item-card/item-card.component.jsx
@@ -1,7 +1,15 @@
-export const ItemCard = (name, imageURL, band, genre, price) => {
+import './item-card.styles.scss'
+
+export const ItemCard = ( {item} ) => {
+    const {name, image, band, genre, price} = item;
     return (
-        <div class='item-card' style={{backgroundImage: `url(${imageURL})`}}>
-            {name}
+        <div class='item-card category-container'>
+        <div className='background-image' style={{backgroundImage:`url(${image})`}}>
+ 
+         </div>
+         <div>
+        {name}
+        </div>
         </div>
     )
 }
--- client/src/routes/shop/categories-main.component.jsx
+++ client/src/routes/shop/categories-main.component.jsx
@@ -2,9 +2,11 @@ import { useContext, Fragment, useEffect } from 'react'
 import { ItemCard } from '../../components/item-card/item-card.component';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCategories } from '../../store/categories/categories.action';
+import './categories-main.styles.scss'
+ 
 
 export const CategoriesMain = () => {
-    const genresList = ['Rock','Pop','Jazz','Classical','Hip hop'];
+    const genresList = ['Rock','Pop','Jazz','Classical','Hip_hop'];
     const categoriesData = useSelector((state) =>  state.categories.categoriesData )
     const dispatch = useDispatch();
     useEffect(() => {
@@ -20,12 +22,36 @@ export const CategoriesMain = () => {
         <Fragment>
            
             {  
-             genresList.map((genre) => { console.log(categoriesData, 'C-D'); return(categoriesData{genre})}) 
-              //todo: dodumat kak vivodit po genre
-            }
+             genresList.map((genre) => 
+             { 
+              console.log(categoriesData, 'C-D'); 
+              return( 
+              <div> 
+                <span className='genreTitle'> 
+                <h1>{genre}</h1> 
+                </span>
+                <div className='items-list'> 
+             {categoriesData.map((obj) => 
+             {
+              if (genre === obj.genre) 
+              {
+                return (
+                 
+                  <ItemCard key = {obj.id} item = {obj} ></ItemCard>
+                  
+                )
+              }
+             })
+             }
+             </div>
+             </div>) 
+               
+            })
+          }
+            
           
   
-   
+          
         </Fragment>
       )
     }

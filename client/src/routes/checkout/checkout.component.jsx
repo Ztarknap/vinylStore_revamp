333b190ff167560c43286a706aacf82f7ec3f414
ztarknap <ztarknap>
2024-01-15 20:03:52 +0700

    put useffect to a component, need to redo

--- client/src/routes/checkout/checkout.component.jsx
+++ client/src/routes/checkout/checkout.component.jsx
@@ -1,20 +1,24 @@
 import { useSelector } from "react-redux"
 import { useEffect } from "react"
-
+import { useState } from "react"
 import { CartItem } from "../../components/cart-item/cart-item.component"
 
+
+//ploho, zasunut v useEffect s sobitiem
+ const MakePurchase = () => {
+    const [resp, setResp] = useState();
+    useEffect(() => {
+         
+        fetch("/api/purchase/makePurchase")
+        .then((res) => res.json())
+        .then((data) => { setResp(data)});
+        }, []);
+    return resp;
+}
 export const Checkout = () => {
 
-        //peremestit useeffect v hook
-    const makePurchase = () => {
-        useEffect(() => {
          
-            fetch("/api/purchase/makePurchase")
-            .then((res) => res.json())
-            .then((data) => { console.log('aa');});
-             
-            }, []);
-    }
+     
 
 
 
@@ -23,7 +27,7 @@ export const Checkout = () => {
     <div>
         {cartItems.map((item) => {return <CartItem key={item._id} item = {item}/>})} 
         <div>
-            <button onClick={makePurchase()}>Make a purchase</button>
+            <button onClick={() => MakePurchase()}>Make a purchase</button>
         </div>
     </div>)
 }

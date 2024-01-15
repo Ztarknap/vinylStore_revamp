b0c0a449b53472ecb1418d685832f17b17661f4a
ztarknap <ztarknap>
2024-01-15 19:18:29 +0700

    deleting comments

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -2,7 +2,7 @@ import {CategoryItem} from '../category-item/category-item.component'
 import './directory.styles.scss'
 
 export const Directory = ({categories}) => {
-    console.log(categories, ' categories ');
+     
     return(
     <div className='categories-container'>
         {categories.map((category) =>  <CategoryItem key = {category.id} category={category}/>  )}
